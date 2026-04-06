35a682fc427d4c54f2860b223236a3caf174e386
Paul Emanuel Contreras Covarrubias <83374367+pol-cova@users.noreply.github.com>
2026-04-06 16:58:47 -0600

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        int freq[26] = {0};
+        for(int i=0;i<s.size(); i++){
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+        for(auto n: freq){
+            if(n!=0) return false;
+        }
+        return true;
+    }
+};
