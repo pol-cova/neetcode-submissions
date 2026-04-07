25a1dbe27883f423edc8de1eb2d767fcc36f7888
Paul Emanuel Contreras Covarrubias <83374367+pol-cova@users.noreply.github.com>
2026-04-06 20:12:09 -0600

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int dfs(TreeNode* node){
+        if(!node) return 0;
+        int l = dfs(node->left);
+        int r = dfs(node->right);
+        if(l == -1 or r == -1) return -1;
+        if(abs(l - r) > 1) return -1;
+        return 1+max(l,r);
+    }
+    bool isBalanced(TreeNode* root) {
+        return dfs(root)!= -1;
+    }
+};
